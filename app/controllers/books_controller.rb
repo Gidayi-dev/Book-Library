d209ce1c3b027652ac4644d1bce9480101f3b40d
Millyannah Gidayi <millyannahi@gmail.com>
2025-02-17 00:53:32 -0800

    fixing book path, creating a book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,47 +1,50 @@
 class BooksController < ApplicationController
+    before_action :set_book, only: [:show, :edit, :update, :destroy]
+  
     def index
-        @books = Book.all
+      @books = Book.all
     end
-
+  
     def show
-        @book = Book.find(params[:id])
     end
-
+  
     def new
-        @book = Book.new
+      @book = Book.new
     end
-
+  
     def create
-        @book = Book.new(book_params)
-        if @book.save
-            redirect_to @book, notice: 'Book was successfully created.'
-        else
-            render :new
-        end
+      @book = Book.new(book_params)
+      if @book.save
+        redirect_to books_path, notice: "Book successfully added!"
+      else
+        render :new
+      end
     end
-
+  
     def edit
-        @book = Book.find(params[:id])
     end
-
-    def update 
-        @book = Book.find(params[:id])
-        if @book.update(book_params)
-            redirect_to @book, notice: 'Book was successfully updated.'
-        else
-            render :edit
-        end
+  
+    def update
+      if @book.update(book_params)
+        redirect_to books_path, notice: "Book successfully updated!"
+      else
+        render :edit
+      end
     end
-
+  
     def destroy
-        @book = Book.find(params[id])
-        @book.destroy
-        redirect_to books_url, norice: 'Book was successfully destroyed.'
+      @book.destroy
+      redirect_to books_path, notice: "Book deleted!"
     end
-
+  
     private
-
-    def book_params
-        params.require(:book).permit(:title, :author, :description, :status)
+  
+    def set_book
+      @book = Book.find(params[:id])
     end
-end
+  
+    def book_params
+      params.require(:book).permit(:title, :author, :description, :status)
+    end
+  end
+  
