a9c29e4511f2d68c53035df41a3bbefa9ac57921
Millyannah Gidayi <millyannahi@gmail.com>
2025-02-16 22:42:05 -0800

    setting up book lending library

---
+++ app/models/book.rb
@@ -0,0 +1,6 @@
+class Book < ApplicationRecord
+    has_many :lending_histories, dependent: :destroy
+  
+    validates :title, :author, :status, presence: true
+    validates :status, inclusion: { in: %w[available borrowed] }
+  end
