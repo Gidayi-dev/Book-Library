ed54d52235bdcf8cf4e7e6118f42088ab9fbed4c
Millyannah Gidayi <millyannahi@gmail.com>
2025-02-16 23:48:45 -0800

    controller test

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
-  test 'should not save book without title' do
-    book = Book.new(author: 'Author', status: 'available')
-    assert_not book.save, 'Saved book without a title'
+  test 'should get index' do
+    get books_url
+    assert_response :success
   end
   # test "the truth" do
   #   assert true
