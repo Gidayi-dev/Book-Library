4a59147f84bc2b240905697022e430be36a40867
Millyannah Gidayi <millyannahi@gmail.com>
2025-02-16 23:43:22 -0800

    model test

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
+  test 'should not save book without title' do
+    book = 'should not save book without title' do
+    assert_not book.save, 'Saved book without a title'
+  end
   # test "the truth" do
   #   assert true
   # end
