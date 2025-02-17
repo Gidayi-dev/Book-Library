232e93c30aaf5dbd5d725232516d45ee1189a81a
Millyannah Gidayi <millyannahi@gmail.com>
2025-02-16 23:20:05 -0800

    controllers (lending histories)

--- app/controllers/lending_histories_controller.rb
+++ app/controllers/lending_histories_controller.rb
@@ -1,2 +1,17 @@
 class LendingHistoriesController < ApplicationController
+    def borrow
+        @book = Book.find(params[:id])
+        @book.update(status: 'borrowed')
+        LendingHistory.create(book: @book, borrower_name: params[:borrower_name], borrowed_at: Time.now)
+        redirect_to @book, notice: 'Book borrowed successfully.'
+    end
+
+    def return
+        @book = Book.find(params[:book_id])
+        @book.update(status: 'available')
+        lending_history = @book.lending_histories.where(returned_at: nill).last
+        lending_history.update(returned_at: Time.now)
+        redirect_to @book, notice: 'Book returned successfully.'
+    end
+    
 end
